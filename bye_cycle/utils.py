f87859b4594fcdd0c47d39cae826a3b6c905eeaf
mehradans <mehrad.ansari@tri.global>
2022-06-22 14:45:16 +0000

    added notebook and more functionality to utils

--- bye_cycle/utils.py
+++ bye_cycle/utils.py
@@ -17,22 +17,22 @@ def interpol_cycle(cell_cycle_data, columns=['voltage', 'current', 'cycle_index'
     return np.array(interpol_cell_cycle_data).T
 
 
-def clean_cycle_data(cell_data, cycle_number):
-    selected_cell_data = cell_data.structured_data[['voltage', 'current', 'discharge_capacity', 'cycle_index']].dropna()
+def clean_cycle_data(cell_data, cycle_number, columns=['voltage', 'current', 'cycle_index', 'discharge_capacity']):
+    selected_cell_data = cell_data.structured_data[columns].dropna()
     # Getting rid of 0 dishcarge data
     clean_cell_data_cycle_number = selected_cell_data.loc[(selected_cell_data['cycle_index'] == cycle_number) 
                                               & (selected_cell_data['discharge_capacity']!=0)]
-    return clean_cell_data_cycle_number[['voltage', 'current', 'cycle_index']]
+    return clean_cell_data_cycle_number[columns]
 
-def prep_features_per_cell(cell, n_points=100):
+def prep_features_per_cell(cell, n_points=100, columns=['voltage', 'current', 'cycle_index'], interpol_kind='linear'):
     max_cycle_idx = max(cell.structured_data['cycle_index'])
-    interp_clean_cell = [] #np.empty((max_cycle_idx, max_time_idx, 3))
+    interp_clean_cell = []
     empty_cycles = []
     for i in tqdm(range(max_cycle_idx)):
-        clean_dat = clean_cycle_data(cell, i)
+        clean_dat = clean_cycle_data(cell, i, columns=columns)
         # Looks like there are some missing cycle measurements so ...
-        if clean_dat.shape[0] not in [0,1]:
-            interp_clean_cell.append(interpol_cycle(clean_dat, n_points=n_points))
+        if clean_dat.shape[0] not in [0, 1, 2]:
+            interp_clean_cell.append(interpol_cycle(clean_dat, n_points=n_points, columns=columns, interpol_kind=interpol_kind))
         else:
             empty_cycles.append(i)
     interp_clean_cell = np.array(interp_clean_cell)
