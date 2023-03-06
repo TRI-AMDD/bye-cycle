855022fa55a83cd3d5cb4e12ddf818edffa604d4
mehradans92 <mehrad.ans@gmail.com>
2023-03-05 20:47:42 -0500

    fixed unittests

--- test/test_bye_cycle.py
+++ test/test_bye_cycle.py
@@ -1,6 +1,10 @@
 import unittest
 import bye_cycle
 import numpy as np
+import sys
+import os
+sys.path.append('/tests')
+print(os.getcwd())
 
 class TestFunctionality(unittest.TestCase):
     """Tests code functionality for `bye_cycle`."""
@@ -14,7 +18,9 @@ class TestUtils(unittest.TestCase):
     """Tests for `bye_cycle.utils` methods."""
     def test_clean_cycle_data(self):
         from beep.structure.cli import auto_load_processed
-        test_cell = auto_load_processed('test_files/test_cell_LFP.json')
+        import os
+        print('HI\n')
+        test_cell = auto_load_processed("./test_files/test_cell_LFP.json")
         columns=['voltage', 'current', 'cycle_index', 'discharge_capacity']
         cycle_index = 5
         clean_structured_data_tested_cell = bye_cycle.clean_cycle_data(test_cell,  cycle_index,
@@ -22,3 +28,5 @@ class TestUtils(unittest.TestCase):
         assert list(clean_structured_data_tested_cell.columns) == columns
         assert np.unique(clean_structured_data_tested_cell['cycle_index']) == cycle_index
 
+if __name__ == '__main__':
+    unittest.main()
